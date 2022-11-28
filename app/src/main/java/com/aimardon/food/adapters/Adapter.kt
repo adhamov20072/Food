7bc70558c7d806e3aa0d60bd512b3258e6feafde
Alimardon <adhamov20072@gmail.com>
2022-11-28 21:37:21 +0500

    new adapter commit

--- app/src/main/java/com/aimardon/food/adapters/Adapter.kt
+++ app/src/main/java/com/aimardon/food/adapters/Adapter.kt
@@ -6,36 +6,33 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.aimardon.food.databinding.RecyclerItemBinding
+import com.aimardon.food.models.Result
 import com.bumptech.glide.Glide
 
-class Adapter : ListAdapter<Food, Adapter.MyViewHolder>(diffUtil) {
-    class MyViewHolder(val binding: RecyclerItemBinding) : RecyclerView.ViewHolder(binding.root) {}
+class Adapter :ListAdapter<Result,Adapter.MyViewHolder>(diffUtil) {
+    class MyViewHolder(val binding: RecyclerItemBinding):RecyclerView.ViewHolder(binding.root)
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<Food>() {
-            override fun areItemsTheSame(oldItem: Food, newItem: Food): Boolean {
-                return oldItem == newItem
+        val diffUtil=object : DiffUtil.ItemCallback<Result>() {
+            override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+                return oldItem==newItem
             }
 
-            override fun areContentsTheSame(oldItem: Food, newItem: Food): Boolean {
-                return oldItem.id == newItem.id
+            override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
+                return oldItem.id==newItem.id
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(
-            RecyclerItemBinding.inflate(
-                LayoutInflater.from(
-                    parent.context
-                ), parent, false
-            )
+            RecyclerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         )
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val item=getItem(position)
+        val item = getItem(position)
         holder.binding.title.text=item.title
-        holder.binding.description.text=item.summary
+        holder.binding.description.text= item.cuisines.toString()
         Glide.with(holder.itemView).load(item.image).into(holder.binding.imageView)
     }
 }
